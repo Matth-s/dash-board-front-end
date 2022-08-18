c91103f2523ee45aeaed8a4f1abcd08b34704d95
Matth-s <108790362+Matth-s@users.noreply.github.com>
2022-08-18 15:51:17 +0200

    Add files via upload

--- pricing/script.js
+++ pricing/script.js
@@ -1,21 +1,24 @@
 const nav = document.querySelector('.navigable');
 const rond = document.querySelector('.rond');
-
 const prix = document.querySelectorAll('.prix-forfait');
 
 
 nav.addEventListener('click', function(e) {
-    console.log(e.screenX);
-    if(e.x > 536) {
+
+    if(window.innerWidth/2 < e.x) {
         rond.classList.add('droite');
         prix[0].innerHTML = "$192.00";
         prix[1].innerHTML = "$390.00";
         prix[2].innerHTML = "$990.00";
+        nav.classList.add('background-black');
+        rond.classList.add('rond-blanc');
     }
-    else if(e.x <= 536) {
+    else {
         rond.classList.remove('droite');
         prix[0].innerHTML = "$19.00";
         prix[1].innerHTML = "$39.00";
         prix[2].innerHTML = "$99.00";
+        nav.classList.remove('background-black');
+        rond.classList.remove('rond-blanc');
     }
 });
